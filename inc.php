2e98c9aa3d214c27663756da12b12ed681e3963f
Stan Rozenraukh <stan@stanistan.com>
2020-01-14 14:09:46 -0500

    w/ some caching

--- inc.php
+++ inc.php
@@ -69,6 +69,8 @@ class RequestCtx {
     private $stream_opts;
     private $cache = [];
 
+    private const CACHE_VERSION = 'v1';
+
     public function __construct(string $auth_key, string $space) {
 
         if (!$auth_key || !$space) {
@@ -89,15 +91,31 @@ class RequestCtx {
     }
 
     private function req(string $path) : array {
+
+        // first we check to see if this response is
+        // in memory, return it from there,
         if (isset($this->cache[$path])) {
             return $this->cache[$path];
         }
-        $context = stream_context_create($this->stream_opts);
-        $url = $this->base_url . $path;
-        $content = @file_get_contents($url, false, $context);
-        if (!$content) {
-            throw new Exception("Could not fetch content for url: $url");
+
+        $disk_cache_dir = "/tmp/invoice-proxy-cache/" . self::CACHE_VERSION . "/";
+        $disk_cache_path = $disk_cache_dir . md5($path);
+
+        if (!file_exists($disk_cache_path)) {
+            $url = $this->base_url . $path;
+            $context = stream_context_create($this->stream_opts);
+            $content = @file_get_contents($url, false, $context);
+            if (!$content) {
+                throw new Exception("Could not fetch content for url: $url");
+            }
+            if (!is_dir($disk_cache_dir)) {
+                mkdir($disk_cache_dir, 0777, true);
+            }
+            file_put_contents($disk_cache_path, $content);
+        } else {
+            $content = file_get_contents($disk_cache_path);
         }
+
         return $this->cache[$path] = json_decode($content, true);
     }
 
