02a43adb5b5df800b1d68ebde96cfad95d92b685
Stan Rozenraukh <stan@stanistan.com>
2020-01-14 17:38:24 -0500

    more shauflin arund

--- main.php
+++ main.php
@@ -1,29 +1,10 @@
 <?php declare(strict_types=1);
-
-setlocale(LC_MONETARY, 'en_US.UTF-8');
-
+//
+//
+//
+// This should be run as the main entrypoint for some server.
+//
+//
+//
 require_once __DIR__ . '/src/inc.php';
-
-// main handler for the request/response structure
-function route(string $route) : Response {
-
-    $ctx = new FetchCtx($_ENV['AIRTABLE_KEY'], $_ENV['AIRTABLE_APP']);
-
-    // extremely bare-bones router
-    $pieces = array_values(array_filter(explode('/', $route)));
-    switch (count($pieces)) {
-    case 2:
-        [ $first, $second ] = $pieces;
-        switch ($first) {
-        case 'invoice':
-            $invoice = pipelines\invoice($ctx);
-            return new Response(200, $invoice($second));
-            break;
-        }
-    }
-
-    // if nothing matched, we send back a 404
-    return new Response(404, [ 'error' => 'Invalid route: ' . $route ]);
-}
-
 route($_SERVER['PHP_SELF'])->send();
--- src/inc.php
+++ src/inc.php
@@ -1,7 +1,33 @@
 <?php declare(strict_types=1);
 
+setlocale(LC_MONETARY, 'en_US.UTF-8');
+
 require_once __DIR__ . '/transforms.php';
 require_once __DIR__ . '/Response.php';
 require_once __DIR__ . '/Pipeline.php';
 require_once __DIR__ . '/FetchCtx.php';
 require_once __DIR__ . '/pipelines.php';
+
+//
+// main handler for the request/response structure
+function route(string $route) : Response {
+
+    $ctx = new FetchCtx($_ENV['AIRTABLE_KEY'], $_ENV['AIRTABLE_APP']);
+
+    // extremely bare-bones router
+    $pieces = array_values(array_filter(explode('/', $route)));
+    switch (count($pieces)) {
+    case 2:
+        [ $first, $second ] = $pieces;
+        switch ($first) {
+        case 'invoice':
+            $invoice = pipelines\invoice($ctx);
+            return new Response(200, $invoice($second));
+            break;
+        }
+    }
+
+    // if nothing matched, we send back a 404
+    return new Response(404, [ 'error' => 'Invalid route: ' . $route ]);
+}
+
