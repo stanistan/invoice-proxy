01d6f2b1a3bdff80b2ee5819e44f9ed8bdbf1c2c
Stan Rozenraukh <stan@stanistan.com>
2020-01-27 10:30:58 -0500

    with type aliases

--- src/schema.rs
+++ src/schema.rs
@@ -3,7 +3,7 @@ use crate::transform::*;
 
 gen_airtable_schema! {
 
-    mod invoice_rate_unit("Invoice Units") {
+    mod invoice_rate_unit("Invoice Units") as InvoiceUnit {
         "Name" => fn name(String) -> String { id },
     } {
         /// Extract the name from the rate unit.
@@ -12,7 +12,7 @@ gen_airtable_schema! {
         }
     }
 
-    mod invoice_item_rate("Invoice Rates") {
+    mod invoice_item_rate("Invoice Rates") as InvoiceRate {
         "Name" => fn name(String) -> String { id },
         "Notes" => fn notes(Option<String>) -> Option<String> { id },
         "Rate" => fn rate(u32) -> u32 { copy },
@@ -24,32 +24,32 @@ gen_airtable_schema! {
         },
     }
 
-    mod invoice_item("Invoice Item") {
+    mod invoice_item("Invoice Item") as InvoiceItem {
         "Date" => fn date(String) -> String { id },
         "Description" => fn description(String) -> String { id },
         "Quantity" => fn quantity(u32) -> u32 { copy },
         "Amount" => fn amount(u32) -> String { money },
-        "Invoice Rate" => fn rate(IDs) -> invoice_item_rate::Mapped {
+        "Invoice Rate" => fn rate(IDs) -> InvoiceRate {
             first,
             invoice_item_rate::one::get,
             invoice_item_rate::one::map
         },
     }
 
-    mod invoice_client("Clients") {
+    mod invoice_client("Clients") as InvoiceClient {
         "Company" => fn company(String) -> String { id },
         "ContactEmail" => fn contact_email(String) -> String { id },
         "ContactName" => fn contact_name(String) -> String { id },
         "Website" => fn website_url(String) -> String { id },
     }
 
-    mod invoice_from("Me") {
+    mod invoice_from("Me") as InvoiceFrom {
         "Name" => fn name(String) -> String { id },
         "Email" => fn email(String) -> String { id },
         "Address" => fn address(String) -> Vec<String> { split_lines },
     }
 
-    mod invoice("Invoice") {
+    mod invoice("Invoice") as Invoice {
         "ID" => fn id(u32) -> u32 { copy },
         "Invoice Number" => fn number(String) -> String { id },
         "Notes" => fn notes(Option<String>) -> Option<String> { id },
@@ -58,17 +58,17 @@ gen_airtable_schema! {
         "Sent" => fn was_sent(MaybeBool) -> bool { force_bool },
         "Paid" => fn was_paid(MaybeBool) -> bool { force_bool },
         "Total Amount" => fn total(u32) -> String { money },
-        "From" => fn from(IDs) -> invoice_from::Mapped {
+        "From" => fn from(IDs) -> InvoiceFrom {
             first,
             invoice_from::one::get,
             invoice_from::one::map
         },
-        "Client" => fn client(IDs) -> invoice_client::Mapped {
+        "Client" => fn client(IDs) -> InvoiceClient {
             first,
             invoice_client::one::get,
             invoice_client::one::map
         },
-        "Invoice Item" => fn items(IDs) -> Vec<invoice_item::Mapped> {
+        "Invoice Item" => fn items(IDs) -> Vec<InvoiceItem> {
             invoice_item::many::get,
             invoice_item::many::map
         },
