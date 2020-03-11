923a113c2827078babaa33e1543010c5c84a568d
Stan Rozenraukh <stan@stanistan.com>
2020-03-11 15:44:43 -0400

    can provide a HOST, not just a port

--- rustlang/src/main.rs
+++ rustlang/src/main.rs
@@ -1,3 +1,5 @@
+use std::net::SocketAddr;
+
 mod airtable;
 mod error;
 mod gen_schema;
@@ -7,24 +9,28 @@ mod transform;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+
+    //
     // make sure we can airtable, yo, that we have the
     // right env setup/permissions here.
     let ctx = airtable::FetchCtx::from_env()?;
 
+    //
     // get our server port from the environment, default is 3000
-    let port: u16 = std::env::var("PORT")
-        .unwrap_or_else(|_| "3000".to_string())
+    let address: SocketAddr = std::env::var("HOST")
+        .unwrap_or_else(|_| "127.0.0.1:3000".to_string())
         .parse()?;
-    // ^^^^^^^^^^^^
-    // TODO: make this take a server address thing that we can parse
 
+    //
     // make our context async ready
     let ctx = ctx::wrap_ctx(ctx);
 
+    //
     // grab the generated router
     let router = schema::route(ctx);
 
-    println!("Will be serving the app on 127.0.0.1:{}", port);
-    warp::serve(router).run(([127, 0, 0, 1], port)).await;
+    println!("Will be serving the app on {}", address);
+    warp::serve(router).run(address).await;
+
     Ok(())
 }
